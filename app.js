d6c4769ba4e1e19616c5007dce482a11f3fc332f
antonoww29 <mar2007to@gmail.com>
2025-12-12 16:03:39 +0200

    Add App.js with Task2 component example

---
+++ app.js
@@ -0,0 +1,15 @@
+// App.js
+return (
+  <div className="App">
+    <h1>Props Overview → 'children' demo</h1>
+
+    <Task2 
+      firstName="Ivan" 
+      lastName="Ivanovski" 
+      title="Teacher" 
+      image={reactLogo}
+    >
+      This is the 'children' prop
+    </Task2>
+  </div>
+);
